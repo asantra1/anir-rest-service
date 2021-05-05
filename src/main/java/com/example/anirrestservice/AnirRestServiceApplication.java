1a1f7d6a687f86edb386e776af45a53b48233e2c
Anirban Santra <>
2021-05-05 22:47:14 +0530

    commit the newly added files

---
+++ src/main/java/com/example/anirrestservice/AnirRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.anirrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnirRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnirRestServiceApplication.class, args);
+	}
+
+}
